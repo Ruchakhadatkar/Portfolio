68ad15d214ef42e96c24a546a4438a73f254f80a
Rucha Khadatkar <ruchakhadatkar83@gmail.com>
2024-02-03 16:24:50 +0530

    fixed bugs

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+window.addEventListener('scroll', function () {
+  const scrollY = window.scrollY;
+  const rotationValue = scrollY / 1; // Adjust the division factor for the desired rotation speed
+
+  document.getElementById('rotatingImage').style.transform = `rotate(${rotationValue}deg)`;
+});
